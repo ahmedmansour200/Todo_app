0a4bd25e1ae4f71cd545971f0f1678ef6e1930e9
ahmedmansour200 <ahmedmansour10122000@gmail.com>
2025-07-10 23:28:06 +0300

    fix: improve layout and styling of TableTodo component; enhance responsiveness and detail display

--- components/TableTodo.tsx
+++ components/TableTodo.tsx
@@ -13,42 +13,56 @@ import TodoAction from "./TodoAction";
 const TableTodo = ({todo} : {todo:ITodo[]}) => {
    
   return (
-   <Table>
-      <TableHeader>
-        <TableRow>
-          <TableHead className="w-[100px]">Counter</TableHead>
-          <TableHead>Title</TableHead>
-          <TableHead>Body</TableHead>
-          <TableHead className="text-right">Completed</TableHead>
-          <TableHead className="text-right">Action</TableHead>
-        </TableRow>
-      </TableHeader>
-      <TableBody>
-        {todo && todo.map((todo, index) => (
-          <TableRow key={todo.id}>
-            <TableCell className="font-medium">{index + 1}</TableCell>
-            <TableCell>{todo.title}</TableCell>
-            <TableCell >
-                <p className="text-sm text-muted-foreground ">
-                    {todo.body && todo.body.length > 16 ? `${todo.body.slice(0, 16)}...` : todo.body}
-                </p>
-                </TableCell>
-            <TableCell className="text-right">
-                {todo.completed ? (<Badge   className="text-xs"> Completed </Badge> ):
-                ( <Badge  variant="secondary" className="text-xs"> UnComplete</Badge> )    
-            }</TableCell>
-            <TableCell className="text-right">
-              <TodoAction todo={todo}/>
-            </TableCell>
-          </TableRow>
-        ))}
-      </TableBody>
-      <TableFooter>
-        <TableRow>
-          <TableCell colSpan={4}>Total</TableCell>
-          <TableCell className="text-end">{todo.length > 0 ? todo.length : "The Empty Todo"}</TableCell>
-        </TableRow>
-      </TableFooter>
+    <Table>
+        <TableHeader className="hidden md:table-header-group">
+            <TableRow>
+                <TableHead className="w-[100px]">Counter</TableHead>
+                <TableHead>Title</TableHead>
+                <TableHead>Body</TableHead>
+                <TableHead className="text-right">Completed</TableHead>
+                <TableHead className="text-right">Action</TableHead>
+            </TableRow>
+        </TableHeader>
+
+        <TableBody className="grid grid-cols-1 gap-4 md:table-row-group">
+            {todo && todo.map((todo, index) => (
+                <TableRow key={todo.id} className="md rounded-lg p-4 grid grid-cols-2 gap-x-4 md:table-row">
+                    
+                    <TableCell className="font-medium col-span-2 md:col-span-1 md:table-cell">
+                        <span className="font-bold md:hidden">Counter : </span>{index + 1}
+                    </TableCell>
+                    <TableCell className="font-medium col-span-2 md:col-span-1 md:table-cell">
+                        <span className="font-bold md:hidden">Title : </span>{todo.title}
+                    </TableCell>
+
+                    <TableCell className="col-span-2 md:col-span-1 md:table-cell">
+                        <span className="font-bold md:hidden">Details : </span>
+                        {todo.body ?  (todo.body.length > 35 ? `${todo.body.slice(0, 35)}...`: todo.body):"Not Available"}
+                    </TableCell>
+
+                    <TableCell className="text-left md:text-right">
+                        <span className="font-bold md:hidden">Status : </span>
+                        {todo.completed ? (
+                            <Badge variant="destructive" className="text-xs">Completed</Badge>
+                        ) : (
+                            <Badge  className="text-xs">UnComplete</Badge>
+                        )}
+                    </TableCell>
+                    <TableCell className="col-span-2 md:col-span-1 md:table-cell">
+                      <div className="flex items-end justify-between md:justify-end gap-2">
+                        <p className="font-bold md:hidden">Action : </p>
+                        <TodoAction todo={todo}/>
+                      </div>
+                    </TableCell>
+                </TableRow>
+            ))}
+        </TableBody>
+        <TableFooter>
+            <TableRow>
+                <TableCell colSpan={4}>Total</TableCell>
+                <TableCell className="text-end">{todo.length > 0 ? todo.length : "The Empty Todo"}</TableCell>
+            </TableRow>
+        </TableFooter>
     </Table>
   )
 }
